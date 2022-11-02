c8c1b739adfb2e61851360920caee58ee8eb2ac0
Byancaam <byaam@hotmail.com>
2022-11-02 20:59:55 -0300

    [dev] Adding cors to application

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
 
 const prisma = new PrismaClient({
   log: ["query"]
@@ -10,13 +11,18 @@ async function bootstrap() {
     logger: true
   });
 
+  // Em produção colocamos o nome do domínio que tem a autorização
+  await fastify.register(cors, {
+    origin: true,
+  })
+
   // http://localhost:3333/o que colocamos depois da barra é o que queremos que seja chamado no get
   fastify.get("/pools/count", async () => {
-    const count = await prisma.pool.count()
+    const count = await prisma.pool.count();
     return { count };
   });
 
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 bootstrap();
